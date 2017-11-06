4b3528a41e8855f5eb73f3c5d804d0c56bf20fb9
Stefan Johnson <sj@scrimpycat.io>
2017-11-07 02:44:10 +1100

    Generate module graph

--- lib/blueprint/plot.ex
+++ lib/blueprint/plot.ex
@@ -1,6 +1,45 @@
 defmodule Blueprint.Plot do
     alias Graphvix.{ Graph, Node, Edge, Cluster }
 
+    def module_graph(blueprint = %Blueprint{ xref: xref }, opts \\ []) do
+        { :ok, graph } = :xref.q(xref, 'ME | A')
+
+        label = Keyword.get(opts, :labeler, &Blueprint.Plot.Label.strip_namespace(Blueprint.Plot.Label.to_label(&1)))
+
+        Graph.new(:module_graph)
+        nodes = Enum.reduce(graph, %{}, fn { a, b }, nodes ->
+            nodes = %{ ^a => node_a, ^b => node_b } = case nodes do
+                %{ ^a => _ } -> Map.put_new(nodes, b, elem(Node.new(label: label.(b)), 0))
+                %{ ^b => _ } -> Map.put_new(nodes, a, elem(Node.new(label: label.(a)), 0))
+                %{ ^a => _, ^b => _ } -> nodes
+                _ ->
+                    Map.put_new(nodes, a, elem(Node.new(label: label.(a)), 0))
+                    |> Map.put_new(b, elem(Node.new(label: label.(b)), 0))
+            end
+
+            Edge.new(node_a, node_b, color: "black")
+            nodes
+        end)
+
+        if Keyword.get(opts, :group, false) do
+            Enum.reduce(nodes, %{}, fn { mod, node_id }, modules ->
+                case modules do
+                    %{ ^mod => nodes } -> %{ modules | mod => [node_id|nodes] }
+                    _ -> Map.put_new(modules, mod, [node_id])
+                end
+            end)
+            |> Enum.each(fn
+                { _, [_] } -> nil
+                { _, nodes } -> Cluster.new(nodes)
+            end)
+        end
+
+        Graph.save
+        Graph.clear
+
+        blueprint
+    end
+
     def call_graph(blueprint = %Blueprint{ xref: xref }, app, opts \\ []) do
         { :ok, graph } = :xref.q(xref, to_charlist("E | #{app}"))
 
