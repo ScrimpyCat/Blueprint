7d3860faf34a403ae4adf43605b74aa9e464e36e
Stefan Johnson <sj@scrimpycat.io>
2017-11-07 02:43:35 +1100

    Convert modules to labels

--- lib/blueprint/plot/label.ex
+++ lib/blueprint/plot/label.ex
@@ -1,5 +1,6 @@
 defmodule Blueprint.Plot.Label do
     def to_label({ mod, fun, arity }), do: "#{mod}.#{fun}/#{arity}"
+    def to_label(mod), do: to_string(mod)
 
     def strip_namespace(label, namespace \\ "Elixir"), do: String.replace_prefix(label, namespace <> ".", "")
 end
