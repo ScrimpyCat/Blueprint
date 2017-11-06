bda85bf0f30e87d18818b655f96c6c8a68f70f7a
Stefan Johnson <sj@scrimpycat.io>
2017-11-07 04:59:15 +1100

    Move combines into separate functions

--- lib/blueprint/plot/graph.ex
+++ lib/blueprint/plot/graph.ex
@@ -1,16 +1,25 @@
 defmodule Blueprint.Plot.Graph do
+    defp add_node(nodes, node, label), do: Map.put_new(nodes, node, elem(Graphvix.Node.new(label: label.(node)), 0))
+
+    defp add_module(modules, mod, node_id) do
+        case modules do
+            %{ ^mod => nodes } -> %{ modules | mod => [node_id|nodes] }
+            _ -> Map.put_new(modules, mod, [node_id])
+        end
+    end
+
     def to_dot(graph, opts \\ []) do
         label = Keyword.get(opts, :labeler, &Blueprint.Plot.Label.strip_namespace(Blueprint.Plot.Label.to_label(&1)))
 
         Graphvix.Graph.new(self())
         nodes = Enum.reduce(graph, %{}, fn { a, b }, nodes ->
             nodes = %{ ^a => node_a, ^b => node_b } = case nodes do
-                %{ ^a => _ } -> Map.put_new(nodes, b, elem(Graphvix.Node.new(label: label.(b)), 0))
-                %{ ^b => _ } -> Map.put_new(nodes, a, elem(Graphvix.Node.new(label: label.(a)), 0))
+                %{ ^a => _ } -> add_node(nodes, b, label)
+                %{ ^b => _ } -> add_node(nodes, a, label)
                 %{ ^a => _, ^b => _ } -> nodes
                 _ ->
-                    Map.put_new(nodes, a, elem(Graphvix.Node.new(label: label.(a)), 0))
-                    |> Map.put_new(b, elem(Graphvix.Node.new(label: label.(b)), 0))
+                    add_node(nodes, a, label)
+                    |> add_node(b, label)
             end
 
             Graphvix.Edge.new(node_a, node_b, color: "black")
@@ -18,11 +27,9 @@ defmodule Blueprint.Plot.Graph do
         end)
 
         if Keyword.get(opts, :group, false) do
-            Enum.reduce(nodes, %{}, fn { mod, node_id }, modules ->
-                case modules do
-                    %{ ^mod => nodes } -> %{ modules | mod => [node_id|nodes] }
-                    _ -> Map.put_new(modules, mod, [node_id])
-                end
+            Enum.reduce(nodes, %{}, fn
+                { { mod, _, _ }, node_id }, modules -> add_module(modules, mod, node_id)
+                { mod, node_id }, modules -> add_module(modules, mod, node_id)
             end)
             |> Enum.each(fn
                 { _, [_] } -> nil
