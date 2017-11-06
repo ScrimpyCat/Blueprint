3e08b72abf731e2b7f0fe6cb8b680029b70a5430
Stefan Johnson <sj@scrimpycat.io>
2017-11-07 04:17:44 +1100

    Add detail option

--- lib/blueprint/plot.ex
+++ lib/blueprint/plot.ex
@@ -1,6 +1,11 @@
 defmodule Blueprint.Plot do
     def application_graph(blueprint = %Blueprint{ xref: xref }, opts \\ []) do
-        { :ok, graph } = :xref.q(xref, 'AE | A')
+        query = case Keyword.get(opts, :detail, :high) do
+            :low -> 'AE ||| A'
+            :high -> 'AE | A'
+        end
+
+        { :ok, graph } = :xref.q(xref, query)
 
         Blueprint.Plot.Graph.to_dot(graph, opts)
         |> Blueprint.Plot.Graph.save!("application_graph.dot")
@@ -9,7 +14,12 @@ defmodule Blueprint.Plot do
     end
 
     def module_graph(blueprint = %Blueprint{ xref: xref }, opts \\ []) do
-        { :ok, graph } = :xref.q(xref, 'ME | A')
+        query = case Keyword.get(opts, :detail, :high) do
+            :low -> 'ME ||| A'
+            :high -> 'ME | A'
+        end
+
+        { :ok, graph } = :xref.q(xref, query)
 
         Blueprint.Plot.Graph.to_dot(graph, opts)
         |> Blueprint.Plot.Graph.save!("module_graph.dot")
@@ -18,7 +28,12 @@ defmodule Blueprint.Plot do
     end
 
     def call_graph(blueprint = %Blueprint{ xref: xref }, app, opts \\ []) do
-        { :ok, graph } = :xref.q(xref, to_charlist("E | #{app}"))
+        query = case Keyword.get(opts, :detail, :high) do
+            :low -> to_charlist("E ||| #{app}")
+            :high -> to_charlist("E | #{app}")
+        end
+
+        { :ok, graph } = :xref.q(xref, query)
 
         Blueprint.Plot.Graph.to_dot(graph, opts)
         |> Blueprint.Plot.Graph.save!("call_graph.dot")
