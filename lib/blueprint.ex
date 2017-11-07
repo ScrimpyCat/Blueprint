36510c70a7c1df20c34104f9a975fce5a254c92c
Stefan Johnson <sj@scrimpycat.io>
2017-11-08 00:35:33 +1100

    Support greater options for initialising the blueprint

--- lib/blueprint.ex
+++ lib/blueprint.ex
@@ -1,16 +1,32 @@
 defmodule Blueprint do
     defstruct [:xref]
 
+    defp add_app(_, []), do: :ok
+    defp add_app(xref, lib) when is_atom(lib), do: { :ok, _ } = :xref.add_application(xref, :code.lib_dir(lib))
+    defp add_app(xref, path) when is_binary(path) do
+        if File.exists?(Path.join(path, "ebin")) do
+            { :ok, _ } = :xref.add_application(xref, to_charlist(path))
+        else
+            Path.wildcard(Path.join(path, "*/ebin"))
+            |> Enum.each(fn ebin ->
+                length = round((bit_size(ebin) / 8) - 4)
+                <<lib :: binary-size(length), "ebin">> = ebin
+
+                if File.dir?(lib) do
+                    { :ok, _ } = :xref.add_application(xref, to_charlist(lib))
+                end
+            end)
+        end
+    end
+    defp add_app(xref, [h|t]) do
+        add_app(xref, h)
+        add_app(xref, t)
+    end
+
     def new(path) do
         { :ok, xref } = :xref.start([])
 
-        File.ls!(path)
-        |> Enum.each(fn file ->
-            lib = Path.join(path, file)
-            if File.dir?(lib) do
-                :xref.add_application xref, to_charlist(lib)
-            end
-        end)
+        add_app(xref, path)
 
         %Blueprint{ xref: xref }
     end
