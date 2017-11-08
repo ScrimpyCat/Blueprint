4be23cc24b4f99631f94fe5046df40c5b621c427
Stefan Johnson <sj@scrimpycat.io>
2017-11-08 21:56:54 +1100

    Add spec to label

--- lib/blueprint/plot/label.ex
+++ lib/blueprint/plot/label.ex
@@ -1,6 +1,8 @@
 defmodule Blueprint.Plot.Label do
+    @spec to_label(atom  | { atom, atom, integer }) :: String.t
     def to_label({ mod, fun, arity }), do: "#{mod}.#{fun}/#{arity}"
     def to_label(mod), do: to_string(mod)
 
+    @spec strip_namespace(String.t, String.t) :: String.t
     def strip_namespace(label, namespace \\ "Elixir"), do: String.replace_prefix(label, namespace <> ".", "")
 end
