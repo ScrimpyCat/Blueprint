633f2cb66eee3ea547583ee53fa431a1ed4b6451
Stefan Johnson <sj@scrimpycat.io>
2017-11-14 18:48:59 +1100

    Add spec to message

--- lib/blueprint/application/module/message.ex
+++ lib/blueprint/application/module/message.ex
@@ -1,3 +1,5 @@
 defmodule Blueprint.Application.Module.Message do
     defstruct [:target, :interface, :args]
+
+    @type t :: %Blueprint.Application.Module.Message{ target: atom, interface: { atom, atom, integer }, args: any }
 end
