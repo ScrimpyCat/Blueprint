b5473458f13328e93036bcc3fe55d8870e6034b0
Stefan Johnson <sj@scrimpycat.io>
2017-11-22 19:17:51 +1100

    Add docs to message

--- lib/blueprint/application/module/message.ex
+++ lib/blueprint/application/module/message.ex
@@ -1,4 +1,9 @@
 defmodule Blueprint.Application.Module.Message do
+    @moduledoc """
+      A struct containing contents about a message useful for
+      inspecting.
+    """
+
     defstruct [:target, :interface, :args]
 
     @type t :: %Blueprint.Application.Module.Message{ target: atom, interface: { atom, atom, integer }, args: any }
