2d6bd62ce6ffe240f79bfa72260074700b01fec6
Stefan Johnson <sj@scrimpycat.io>
2017-11-24 22:26:02 +1100

    Include simple_markdown dep

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Blueprint.Mixfile do
     defp deps do
         [
             { :graphvix, "~> 0.5.0" },
+            { :simple_markdown, "~> 0.3.0" },
             { :ex_doc, "~> 0.18", only: :dev }
         ]
     end
