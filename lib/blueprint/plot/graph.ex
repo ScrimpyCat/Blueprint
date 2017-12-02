07fa9f40f2f9873b8ddfa9c2cd6e9ce173d94ae4
Stefan Johnson <sj@scrimpycat.io>
2017-12-02 18:54:49 +1100

    Add remaining specs

--- lib/blueprint/plot/graph.ex
+++ lib/blueprint/plot/graph.ex
@@ -7,9 +7,16 @@ defmodule Blueprint.Plot.Graph do
     @type graph_node :: any
     @type meta :: any
     @type connection :: { graph_node, graph_node } | { graph_node, graph_node, meta }
+    @type graph :: [connection]
+    @type labeler :: (graph_node -> String.t)
+    @type styler :: ({ :node, graph_node } | { :connection, connection } -> keyword())
+    @type node_cache :: %{ optional(graph_node) => pos_integer }
+    @type cluster_type :: :app | :mod
 
+    @spec add_node(node_cache, graph_node, labeler, styler) :: node_cache
     defp add_node(nodes, node, label, styler), do: Map.put_new(nodes, node, elem(Graphvix.Node.new(Keyword.merge([label: label.(node)], styler.({ :node, node }))), 0))
 
+    @spec add_module(%{ optional(graph_node) => [pos_integer] }, graph_node, pos_integer) :: %{ optional(graph_node) => [pos_integer] }
     defp add_module(modules, mod, node_id) do
         case modules do
             %{ ^mod => nodes } -> %{ modules | mod => [node_id|nodes] }
@@ -17,6 +24,7 @@ defmodule Blueprint.Plot.Graph do
         end
     end
 
+    @spec define_clusters(node_cache, [cluster_type] | cluster_type | nil) :: node_cache
     defp define_clusters(nodes, nil), do: nodes
     defp define_clusters(nodes, :mod) do
         Enum.reduce(nodes, %{}, fn
@@ -58,6 +66,7 @@ defmodule Blueprint.Plot.Graph do
         define_clusters(nodes, t)
     end
 
+    @spec define_nodes(node_cache, graph_node, graph_node, labeler, styler) :: node_cache
     defp define_nodes(nodes, a, b, label, styler) do
         case nodes do
             %{ ^a => _, ^b => _ } -> nodes
@@ -75,20 +84,20 @@ defmodule Blueprint.Plot.Graph do
       Options can be provided to change the resulting graph. These
       options are:
 
-      * `:labeler` - A function of type `(any -> String.t)`, where
+      * `:labeler` - A function of type `labeler`, where
       the node is passed to the function and is expected to
       receive a string that will be used on the graph to label it
       as a result.
-      * `:styler` - A function of type `(node_element | connection_element -> keyword())`
-      where `node_element` is of type `{ :node, node }` and
-      `connection_element` is of type `{ :connection, connection }`.
+      * `:styler` - A function of type `styler` where a node
+      or connection is passed the function and is expected to
+      return any styling changes to overwrite the defaults with.
     """
-    @spec to_dot([{ node, node } | { node, node, meta }], keyword()) :: String.t
+    @spec to_dot(graph, keyword()) :: String.t
     def to_dot(graph, opts \\ []) do
         styler = Keyword.get(opts, :styler, fn _ -> [color: "black"] end)
         label = Keyword.get(opts, :labeler, &Blueprint.Plot.Label.strip_namespace(Blueprint.Plot.Label.to_label(&1)))
 
-        Graphvix.Graph.new(self())
+        :ok = Graphvix.Graph.new(__MODULE__)
         nodes = Enum.reduce(graph, %{}, fn
             connection = { a, b }, nodes ->
                 nodes = %{ ^a => node_a, ^b => node_b } = define_nodes(nodes, a, b, label, styler)
