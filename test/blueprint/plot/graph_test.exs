cffd19e12d5f8b8347fe9ee2502be6fa8120bb00
Stefan Johnson <sj@scrimpycat.io>
2017-12-03 18:27:58 +1100

    Test groups

--- test/blueprint/plot/graph_test.exs
+++ test/blueprint/plot/graph_test.exs
@@ -97,4 +97,45 @@ defmodule Blueprint.Plot.GraphTest do
             B -> "2"
         end)
     end
+
+    test "app group" do
+        assert String.trim("""
+        digraph G {
+          node_1 [label="A.Foo",color="black"];
+          node_2 [label="B.Bar",color="black"];
+          node_4 [label="A.Bar",color="black"];
+          node_6 [label="C",color="black"];
+
+          node_1 -> node_2 [color="black"];
+          node_2 -> node_4 [color="black"];
+          node_1 -> node_6 [color="black"];
+
+          subgraph cluster_8 {
+            node_1 -> node_4 [style=invis];
+            { rank = \"same\"; node_1; node_4; }
+          }
+        }
+        """) == Blueprint.Plot.Graph.to_dot([{ A.Foo, B.Bar }, { B.Bar, A.Bar }, { A.Foo, C }], group: :app)
+    end
+
+    test "mod group" do
+        assert String.trim("""
+        digraph G {
+          node_1 [label="A.Foo.a/0",color="black"];
+          node_2 [label="B.Bar.b/0",color="black"];
+          node_4 [label="B.Bar.a/0",color="black"];
+          node_5 [label="A.Bar.a/0",color="black"];
+          node_7 [label="C.a/0",color="black"];
+
+          node_1 -> node_2 [color="black"];
+          node_4 -> node_5 [color="black"];
+          node_1 -> node_7 [color="black"];
+
+          subgraph cluster_9 {
+            node_2 -> node_4 [style=invis];
+            { rank = \"same\"; node_2; node_4; }
+          }
+        }
+        """) == Blueprint.Plot.Graph.to_dot([{ { A.Foo, :a, 0 }, { B.Bar, :b, 0 } }, { { B.Bar, :a, 0 }, { A.Bar, :a, 0 } }, { { A.Foo, :a, 0 }, { C, :a, 0 } }], group: :mod)
+    end
 end
