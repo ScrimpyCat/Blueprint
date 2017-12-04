6c62bbfc4165f8ec3c2e397a9a4086cff845ccce
Stefan Johnson <sj@scrimpycat.io>
2017-12-04 23:05:36 +1100

    Update to 0.2.0

--- README.md
+++ README.md
@@ -19,6 +19,6 @@ Installation
 ------------
 ```elixir
 defp deps do
-    [{ :blueprint, "~> 0.1.0" }]
+    [{ :blueprint, "~> 0.2.0" }]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blueprint.Mixfile do
         [
             app: :blueprint,
             description: "A library to visualize various aspects of your application",
-            version: "0.1.0",
+            version: "0.2.0",
             elixir: "~> 1.5",
             start_permanent: Mix.env == :prod,
             deps: deps(),
