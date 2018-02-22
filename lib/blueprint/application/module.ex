8b0b63ef0e7f1fd515bcc798e0d2687cb74b0697
Stefan Johnson <sj@scrimpycat.io>
2018-02-22 22:50:37 +1100

    Add server matching expressions

--- lib/blueprint/application/module.ex
+++ lib/blueprint/application/module.ex
@@ -2,6 +2,29 @@ defmodule Blueprint.Application.Module do
     @moduledoc """
       A struct containing contents from a BEAM module useful for
       inspecting.
+
+      By default messages will only be found when servers explicitly
+      match. This however can be extended by providing custom server
+      matching expressions in the config under the `:servers` key.
+
+      ## Server Match Expressions
+
+      Server match expression should take the form of `{ match, server }`,
+      where `match` is the expression to be matched with (against the
+      function calls server arg), and `server` is the resulting target
+      server.
+
+      The `:servers` key should have a string of the the list of server
+      match expressions.
+
+      An example of this could be:
+
+        config :blueprint,
+            servers: ~S([
+                { { :tuple, _, [{ :atom, _, s }, { :atom, _, :"foo@127.0.0.1" }] }, s }, \# match against a named node, and return whatever server name is bound to 's'
+                { { :call, _, { :atom, _, :get_server }, [] }, Foo }, \# match against a get_server/0 function and return Foo as the server
+                { _, Bar } \# match any argument and return Bar as the server
+            ])
     """
 
     defstruct [path: nil, beam: nil, name: nil, messages: [], server: nil]
@@ -18,6 +41,29 @@ defmodule Blueprint.Application.Module do
             interface: { module, fun, length(args) },
             args: args #TODO: format
         }|messages]
+    end
+    defp messages({ :call, _, { :remote, _, { :atom, _, module }, { :atom, _, fun } }, args = [server_arg|_] }, messages) when module in @server_behaviours and fun in @server_behaviours_sends do
+        Application.get_env(:blueprint, :servers, "[]")
+        |> Code.string_to_quoted!
+        |> Enum.find_value(messages, fn { match, server } ->
+            try do
+                { server, _ } =
+                    quote do
+                        case var!(arg) do
+                            unquote(match) -> unquote(server)
+                        end
+                    end
+                    |> Code.eval_quoted([arg: server_arg])
+
+                [%Blueprint.Application.Module.Message{
+                    target: server,
+                    interface: { module, fun, length(args) },
+                    args: args #TODO: format
+                }|messages]
+            rescue
+                _ -> nil
+            end
+        end)
     end
     defp messages([h|t], messages), do: messages(t, messages(h, messages))
     defp messages(code, messages) when is_tuple(code), do: messages(Tuple.to_list(code), messages)
